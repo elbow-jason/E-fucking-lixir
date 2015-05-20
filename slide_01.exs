e1420ba0f1c8857549925ed96599d17e9e7c9e6a
Jason Goldberger <jlgoldb2@asu.edu>
2015-05-20 14:16:48 -0700

    added 1 and 2

---
+++ slide_01.exs
@@ -0,0 +1,15 @@
+defmodule Presentation do
+  @moduledoc """
+  By Jason Goldberger
+  for Language Roulette
+  June 3rd, 2015
+  """
+
+  def language(:elixir) do
+    true
+  end
+  def language(_) do
+    false
+  end
+
+end
---
+++ slide_02.exs
@@ -0,0 +1,11 @@
+defmodule WhyElixir do
+  def reasons do
+    {
+      "fun",
+      'expressive',
+      :concurrent,
+      'functional',
+      "fault-tolerant"
+    }
+  end
+end
