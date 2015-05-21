e121569ce8238807ad1113270dd9d1179bd33d57
Jason Goldberger <jlgoldb2@asu.edu>
2015-05-20 22:00:28 -0700

    added notes

--- slide_02.exs
+++ slide_02.exs
@@ -1,11 +1,12 @@
 defmodule WhyElixir do
   def reasons do
-    {
-      "fun",
-      'expressive',
-      :concurrent,
-      'functional',
-      "fault-tolerant"
-    }
+    [
+     :fun,
+     :expressive,
+     :concurrent,
+     :functional,
+     :fault_tolerant,
+     :fast,
+    ]
   end
 end
